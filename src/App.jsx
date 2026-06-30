9c1f0dd8fd9a58b856792401bc80a1624f71c178
Prachi Tiwari <tiwariprachi395@gmail.com>
2026-06-30 11:58:47 +0300

    initial commit - react vite setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import First from './components/first'
+// import Prachi from './components/prachi.png'
+function App() {
+  return (
+    <div>
+      <First name="Prachi" img src="{Prachi}"></First>
+      <First name="Maharanapratap"></First>
+      <First name="rani laxmi bai" desc="hello hello"></First>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/first.jsx
@@ -0,0 +1,14 @@
+import React from'react'
+import Prachi from "../components/prachi.png"
+function First(props){
+    return(
+    <div className='first'>
+        <h1 id="tittle">{props.name}</h1>
+        <img  id="img"  alt="Prachi"></img>
+        <p id="desc">{props.desc
+            }
+        </p>
+    </div>
+    )
+}
+export default First;

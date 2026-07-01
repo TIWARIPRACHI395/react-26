e7504a5c9ccd0b7720557e96c8269293595688aa
Prachi Tiwari <tiwariprachi395@gmail.com>
2026-07-01 22:33:12 +0300

    hello

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 function Box(){
     return(
-        <div className='Box'>CLICK ME BUTTON</div>
+        <div className='Box'>CLICK ME BUTTON
+        <h1>hello world</h1>
+        </div>
     )
 }
 export default Box
